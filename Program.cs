e3ee21a80889fd29f7f26b5553f65eb10ec00fe9
cactusjhack <perea.2005jp@gmail.com>
2026-02-17 20:17:48 -0500

    version mia

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+Universidades universidad1 = new Universidades();
+var universidad2 = new Universidades();
+
+public class Sedes
+{
+    public int Id { get; set; }
+    public string Nombre { get; set; }
+    public decimal Capacidad { get; set; }
+    public int Universidad { get; set; }
+
+    public Universidades _Universidad { get; set; }
+}
+
+public class Universidades
+{
+    private int id = 0;
+    private string nombre = "";
+    private DateTime fecha = DateTime.Now;
+    private bool activa = true;
+    private List<Sedes> sedes;
+
+    public Universidades() { }
+
+    public int Id
+    {
+        get { return this.Id; }
+        set { this.id = value; }
+
+    }
+    public String Nombre
+    {
+        get { return this.Nombre; }
+    }
+    public string Dir { get; set; }
+}
