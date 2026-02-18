2e9a9d098222334ad2786575076a240d555b310b
cactusjhack <perea.2005jp@gmail.com>
2026-02-18 18:11:21 -0500

    Actualizacion sedes

--- Program.cs
+++ Program.cs
@@ -1,9 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Clase 17/02/2026");
+Universidades universidad = new Universidades()
+{
+    nombre = "ITM",
+    activa = false,
+};
 
-Universidades universidad1 = new Universidades();
-var universidad2 = new Universidades();
+universidad.id = 1;
+var temp = universidad.id;
 
+universidad.sedes = new List<Sedes>();
+universidad.sedes.Add(new Sedes() { Id = 1, Nombre = "Fraternidad" });
+universidad.sedes.Add(new Sedes() { Id = 2, Nombre = "Robledo" });
+universidad.sedes.Add(new Sedes() { Id = 3, Nombre = "La Floresta" });
+universidad.sedes.Add(new Sedes() { Id = 4, Nombre = "CATA" });
+universidad.sedes.Add(new Sedes() { Id = 5, Nombre = "Castilla" });
+universidad.sedes.Add(new Sedes() { Id = 6, Nombre = "Prado" });
+
+foreach (var elemento in universidad.sedes)
+{
+    Console.WriteLine("id:" + elemento.Id + "," + " Nombre:" + elemento.Nombre);
+}
 public class Sedes
 {
     public int Id { get; set; }
@@ -16,23 +32,22 @@ public class Sedes
 
 public class Universidades
 {
-    private int id = 0;
-    private string nombre = "";
-    private DateTime fecha = DateTime.Now;
-    private bool activa = true;
-    private List<Sedes> sedes;
+    public int id = 0;
+    public string nombre = "";
+    public DateTime fecha = DateTime.Now;
+    public bool activa = true;
+    public List<Sedes> sedes;
 
     public Universidades() { }
 
     public int Id
     {
-        get { return this.Id; }
+        get { return this.id; }
         set { this.id = value; }
 
     }
     public String Nombre
     {
-        get { return this.Nombre; }
+        get { return this.nombre; }
     }
-    public string Dir { get; set; }
 }
